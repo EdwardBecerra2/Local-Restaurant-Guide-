9aa38611c2c723c3f68cc91f71a20ff5aaa9335f
wilsoncheng324 <wilsoncheng0324@gmail.com>
2023-11-30 11:47:46 -0800

    html-css-js-add

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,40 @@
-var APIKey = "R_sNro_9M5MMte44II_lc7lHapNDozfk4koDZwqGa4dUlQCp1TwCUyxbKLXd9cA5IwYSJh8_W06hoVokVsGbNO-Y91AZhyXUpbNw2-611hQVd798jZd_udzI7A1oZXYx"
+// var APIKey = "R_sNro_9M5MMte44II_lc7lHapNDozfk4koDZwqGa4dUlQCp1TwCUyxbKLXd9cA5IwYSJh8_W06hoVokVsGbNO-Y91AZhyXUpbNw2-611hQVd798jZd_udzI7A1oZXYx"
+// Example
+var exampleResults = [
+    { name: 'Restaurant 1', location: '123 Main St' },
+    { name: 'Restaurant 2', location: '456 Main St' },
+    // Add more results as needed
+];
+
+var exampleHistorys = [
+    { name: 'Restaurant 1', location: '123 Main St' },
+    { name: 'Restaurant 2', location: '456 Main St' },
+    // Add more results as needed
+];
+
+function displayResults(results) {
+    var resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = ''; // Clear previous results
+
+    results.forEach(result => {
+        var resultItem = document.createElement('div');
+        resultItem.className = 'result-item';
+        resultItem.textContent = `${result.name} - ${result.location}`;
+        resultsContainer.appendChild(resultItem);
+    });
+}
+
+function displayHistorys(historys) {
+    var historysContainer = document.getElementById('historys');
+    historysContainer.innerHTML = ''; // Clear previous historys
+
+    historys.forEach(historys => {
+        var historysItem = document.createElement('div');
+        historysItem.className = 'historys-item';
+        historysItem.textContent = `${historys.name} - ${historys.location}`;
+        historysContainer.appendChild(historysItem);
+    });
+}
+
+displayResults(exampleResults);
+displayHistorys(exampleResults);
